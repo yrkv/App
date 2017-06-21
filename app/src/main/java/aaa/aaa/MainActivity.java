040a8f856ad962ce3215db9785089bc4d15b2473
yrkv <yegorushka.kuznetsov@gmail.com>
2017-06-21 18:56:00 -0500

    AaaaAAAAaaaaaaaaaaaaaaaaaaaaaAaaaaAaaa

--- app/src/main/java/aaa/aaa/MainActivity.java
+++ app/src/main/java/aaa/aaa/MainActivity.java
@@ -2,7 +2,6 @@ package aaa.aaa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.View;
 import android.widget.TextView;
 
@@ -14,8 +13,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void Aaa(View view) throws InterruptedException {
+    public void Aaa(final View view) throws InterruptedException {
         setContentView(R.layout.next_activity);
-        ((TextView) findViewById(R.id.aaaText)).setText(new char[] {'a', 'b', 'c'}, 0, 3);
+        final char[] chars = new char[100000];
+        for (int i = 0; i < chars.length; i++) {
+            if (Math.random() < 0.9)
+                chars[i] = 'a';
+            else
+                chars[i] = 'A';
+        }
+
+        final TextView textView = (TextView) findViewById(R.id.aaaText);
+        Runnable myRunnable = new Runnable() {
+            int lastLen = 0;
+            @Override
+            public void run() {
+                long time = System.currentTimeMillis();
+                while (true) {
+                    int diff = (int) (System.currentTimeMillis() - time);
+                    final int len = (diff / 10 > 0) ? diff / 10 : 1;
+                    if (len > lastLen) {
+                        textView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                textView.setText(chars, 0, len);
+                            }});
+                        lastLen = len;
+                    }
+                }
+            }
+        };
+
+        Thread myThread = new Thread(myRunnable);
+        myThread.start();
     }
 }
