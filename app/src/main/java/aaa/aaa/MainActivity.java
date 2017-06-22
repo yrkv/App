0febcf299882fc93dd9588951873727d168460a7
pwnzor1337 <kaleb.mcclenathan@gmail.com>
2017-06-22 15:06:25 -0500

    Changed shit doesnt work

--- app/src/main/java/aaa/aaa/MainActivity.java
+++ app/src/main/java/aaa/aaa/MainActivity.java
@@ -14,32 +14,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Aaa(final View view) throws InterruptedException {
-        setContentView(R.layout.next_activity);
-        final char[] chars = new char[100000];
-        for (int i = 0; i < chars.length; i++) {
-            if (Math.random() < 0.9)
-                chars[i] = 'a';
-            else
-                chars[i] = 'A';
-        }
-
-        final TextView textView = (TextView) findViewById(R.id.aaaText);
         Runnable myRunnable = new Runnable() {
-            int lastLen = 0;
             @Override
             public void run() {
-                long time = System.currentTimeMillis();
                 while (true) {
-                    int diff = (int) (System.currentTimeMillis() - time);
-                    final int len = (diff / 10 > 0) ? diff / 10 : 1;
-                    if (len > lastLen) {
-                        textView.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                textView.setText(chars, 0, len);
-                            }});
-                        lastLen = len;
-                    }
+                    setContentView(R.layout.next_activity);
                 }
             }
         };
