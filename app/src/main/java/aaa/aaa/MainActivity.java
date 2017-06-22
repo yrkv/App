f50c2cd415443165fe21da5c7bfc0bbb70387e83
yrkv <yegorushka.kuznetsov@gmail.com>
2017-06-22 15:37:45 -0500

    fix

--- app/src/main/java/aaa/aaa/MainActivity.java
+++ app/src/main/java/aaa/aaa/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 while (playing) {
-                    
+
                 }
             }
         };
