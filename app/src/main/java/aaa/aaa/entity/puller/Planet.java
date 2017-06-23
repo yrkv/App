76795fa760fd8b5d95a02b6820c444de4f88b586
yrkv <yegorushka.kuznetsov@gmail.com>
2017-06-23 14:48:35 -0500

    random planet

--- app/src/main/java/aaa/aaa/entity/puller/Planet.java
+++ app/src/main/java/aaa/aaa/entity/puller/Planet.java
@@ -9,9 +9,23 @@ import aaa.aaa.level.Level;
 
 public class Planet extends Puller {
 
-    public Planet(double x, double y, double xVelocity, double yVelocity, Level level) {
+    public Planet(double x, double y, double xVelocity, double yVelocity, float size, Level level) {
         super(x, y, xVelocity, yVelocity, level);
 
-        setImageView(R.drawable.planet1);
+        setImageView(randomPlanet());
+    }
+
+    private int randomPlanet() {
+        int num = (int) (Math.random() * 7 + 1);
+        switch (num) {
+            case 1:return R.drawable.planet1;
+            case 2:return R.drawable.planet2;
+            case 3:return R.drawable.planet3;
+            case 4:return R.drawable.planet4;
+            case 5:return R.drawable.planet5;
+            case 6:return R.drawable.planet6;
+            case 7:return R.drawable.planet7;
+        }
+        return R.drawable.planet1;
     }
 }
