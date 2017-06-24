8317539894c39afef56c967f4ece968735677cef
pwnzor1337 <kaleb.mcclenathan@gmail.com>
2017-06-23 22:41:50 -0500

    Rocket flies normally now

--- app/src/main/java/aaa/aaa/entity/puller/BlackHole.java
+++ app/src/main/java/aaa/aaa/entity/puller/BlackHole.java
@@ -11,9 +11,7 @@ public class BlackHole extends Puller {
 
     public BlackHole(double x, double y, float dir, double xVelocity, double yVelocity, float size, Level level) {
         super(x, y, dir, xVelocity, yVelocity, size, level);
-        System.out.println(getGravity());
         setGravity(true);
-        System.out.println(getGravity());
 
         setImageView(R.drawable.blackhole);
     }
