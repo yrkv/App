21720b457d7e1a8cfa45b7415fa353baba0764a0
yrkv <yegorushka.kuznetsov@gmail.com>
2017-07-05 19:14:51 -0500

    added scrolling to the level select

--- app/src/main/java/aaa/aaa/MainActivity.java
+++ app/src/main/java/aaa/aaa/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import java.util.ArrayList;
 
