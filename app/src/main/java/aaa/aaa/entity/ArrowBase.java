84614436d45bb36df8402e31d85acc67843ca21b
pwnzor1337 <kaleb.mcclenathan@gmail.com>
2017-09-16 21:54:48 -0500

    ermm.. Wtf is this fake entity?

--- app/src/main/java/aaa/aaa/entity/ArrowBase.java
+++ app/src/main/java/aaa/aaa/entity/ArrowBase.java
@@ -1,5 +1,7 @@
 public class ArrowBase() extends EntityBase {
     public ArrowBase() {
-        super(0,0,0,
+        super(0,0,0,0,0,0,new Level);
+        
+        setImageView(R.drawable.planet1);
     }
 }
